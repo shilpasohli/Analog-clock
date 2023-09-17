51e1595fd8d340837d62215e392220df58274a58
shilpasohli <142411110+shilpasohli@users.noreply.github.com>
2023-09-17 15:49:13 +0530

    Add files via upload

---
+++ Analog Clock/script.js
@@ -0,0 +1,30 @@
+const hour = document.getElementById('clock-hour'),
+      minutes = document.getElementById('clock-minutes'),
+      seconds = document.getElementById('clock-seconds')
+
+const clock = () =>{
+    let date = new Date()
+
+    let hh = date.getHours() * 30,
+        mm = date.getMinutes() * 6,
+        ss = date.getSeconds() * 6
+        
+    // We add a rotation to the elements
+    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
+    minutes.style.transform = `rotateZ(${mm}deg)`
+    seconds.style.transform = `rotateZ(${ss}deg)`
+}
+setInterval(clock, 1000)
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bxs-sun'
+
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
